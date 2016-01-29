429cdc1b094f6a58cc100d38dbcf80f483ca6fcf
Darshan <darshanmaiya@outlook.com>
2016-01-29 15:00:12 -0800

    Move project to eclipse

--- src/main/java/webmon/HelloWorld.java
+++ src/main/java/webmon/HelloWorld.java
@@ -1,12 +1,7 @@
 package webmon;
 
-import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
-import java.util.*;
-import java.util.logging.*;
-import com.google.appengine.api.datastore.*;
-import com.google.appengine.api.memcache.*;
 
 @SuppressWarnings("serial")
 public class HelloWorld extends HttpServlet {
