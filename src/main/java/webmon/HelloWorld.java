461973d86388735b770e79eaea4e1aabe8059f8c
Darshan <darshanmaiya@outlook.com>
2016-01-29 14:32:39 -0800

    Initilized project with Hello World

---
+++ src/main/java/webmon/HelloWorld.java
@@ -0,0 +1,24 @@
+package webmon;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.memcache.*;
+
+@SuppressWarnings("serial")
+public class HelloWorld extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+      resp.setContentType("text/html");
+      resp.getWriter().println("<html><body>");
+      resp.getWriter().println("<h2>Hello World</h2>"); //remove this line
+
+      //Add your code here
+
+      resp.getWriter().println("</body></html>");
+  }
+}
