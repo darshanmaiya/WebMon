a2e2dffcd296fb54460fdbc6f4dd4b2afe39160f
Darshan <darshanmaiya@outlook.com>
2016-02-10 20:32:05 -0800

    Replace Jersey Filter with Servlet Filter to fix App Engine error

--- src/main/java/webmon/filters/AuthorizationFilter.java
+++ src/main/java/webmon/filters/AuthorizationFilter.java
@@ -4,24 +4,23 @@ import java.io.IOException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.*;
-import javax.ws.rs.ext.Provider;
 
 import webmon.utils.AuthenticationUtils;
 import webmon.utils.Constants;
 
-@Provider
-public class AuthorizationFilter implements ContainerRequestFilter {
-	@Context private HttpServletRequest request;
-	@Context private HttpServletResponse response;
- 
-    @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-    	
-    	try {
-    		if(!AuthenticationUtils.isLoggedIn(request)) {
+// Implements Filter class
+public class AuthorizationFilter implements Filter  {
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+					  throws IOException, ServletException {
+
+		try {
+    		if(!AuthenticationUtils.isLoggedIn((HttpServletRequest)request)) {
     			 
             	request.setAttribute(Constants.stringErrorCode, Constants.stringError401);
             	request.setAttribute(Constants.stringErrorMessage, Constants.stringError401NotLoggedIn);
@@ -30,7 +29,7 @@ public class AuthorizationFilter implements ContainerRequestFilter {
             	return;
             }
             
-            if(!AuthenticationUtils.isAuthorized(request)) {
+            if(!AuthenticationUtils.isAuthorized((HttpServletRequest)request)) {
             	
             	request.setAttribute(Constants.stringErrorCode, Constants.stringError401);
             	request.setAttribute(Constants.stringErrorMessage, Constants.stringError401Unauthorized);
@@ -41,5 +40,13 @@ public class AuthorizationFilter implements ContainerRequestFilter {
     	} catch (ServletException e) {
     		e.printStackTrace();
     	}
-    }
+
+		// Pass request back down the filter chain
+		chain.doFilter(request,response);
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
 }
